1f01f775e5367d0281cdb751e16cf8eaf7e6438e
Anthony oliveira <antholi0203@gmail.com>
2025-11-05 14:26:33 +0000

    atl

--- lista_array/lista.java
+++ lista_array/lista.java
@@ -1,4 +1,16 @@
-// uma array armazena valores do mesmo tipos
+// uma array armazena valores do mesmo tipos e tem um limiter 
 
 Int[] arrayNumero = new int[5] // o 5 é o numero que pode ser armazenado dentro dela
+
+// Lista aceita qualquer objeto tem um limite de 10 espaço
+
+list<tipo> nome = new list<tipo>();
+.get(0) -> colocar a um numero ele retorna um item da lista
+.add()   -> adiciona na lista
+.remove()-> remove da lista
+.size()  -> tamanho da lista
+.contains() -> verifica se a lista tem um elemento
+.clear() -> limpa a lista
+.indexOf() -> coloca o item que a pessoa quer e ele retorna a posição que ele está
+.String() -> retorna um String com Objeto : dentro do objeto recrever a função
 
