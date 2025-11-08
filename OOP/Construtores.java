7c7555d164395fe843c13b0be26c5969bee3a952
Anthony oliveira <antholi0203@gmail.com>
2025-11-08 23:05:18 +0000

    super

---
+++ OOP/Construtores.java
@@ -0,0 +1,18 @@
+public class Pessoa{
+
+    // Criação dos construtures, quando for instancia essa class, e obrigatorio passa o nome e idade
+    public Pessoa(String nome, int Idade){
+
+    }
+}
+
+
+public class Aluno extends Pessoa{
+
+    public Aluno(String nome, int Idade, String Curso){
+        // o super chama o construtor da class pai
+        super(Nome, Idade)
+    }
+}
+
+
